45c9d35d28fc58fefa14631c87f272381565b113
Tyler Tracy <tylertracy1999@gmail.com>
2024-03-05 17:25:45 -0600

    SVM (but it freezes...)

--- clean-data.py
+++ clean-data.py
@@ -4,6 +4,7 @@ from sklearn.naive_bayes import CategoricalNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.cluster import KMeans
+from sklearn.svm import SVC
 
 
 dataFile = "data/adult.data"
@@ -37,32 +38,6 @@ def cleanData(fileName: str):
     return X, Y
 
 
-def clean2(fileName: str):
-    df = pd.read_csv(fileName, names=headers, na_values=['?'], skipinitialspace=True)
-    df.dropna(inplace=True)
-
-    # Remove all periods from the values
-    for column in categorical_features:
-        df[column] = df[column].str.rstrip('.')
-
-    
-    # for each continious column, map it to if it is greater than the mean of that column
-    for column in continuous_features:
-        df[column] = df[column].astype(int)
-        mean = df[column].mean()
-        df[column] = df[column].map(lambda x: 1 if x > mean else 0)
-
-
-    X = pd.get_dummies(df, drop_first=True)
-
-    # Add missing columns
-    for category in countries:
-        cat_col = "native-country_" + category
-        if cat_col not in X.columns:
-            X[cat_col] = 0
-
-    X = X.sort_index(axis=1)
-    return X
 
 trainX, trainY = cleanData(dataFile)
 testX, testY = cleanData(testFile)
@@ -107,6 +82,32 @@ getStats(model)
 
 ### ============ Part 2 ============
 
+def clean2(fileName: str):
+    df = pd.read_csv(fileName, names=headers, na_values=['?'], skipinitialspace=True)
+    df.dropna(inplace=True)
+
+    # Remove all periods from the values
+    for column in categorical_features:
+        df[column] = df[column].str.rstrip('.')
+
+    # for each continious column, map it to if it is greater than the mean of that column
+    for column in continuous_features:
+        df[column] = df[column].astype(int)
+        mean = df[column].mean()
+        df[column] = df[column].map(lambda x: 1 if x > mean else 0)
+
+
+    X = pd.get_dummies(df, drop_first=True)
+
+    # Add missing columns
+    for category in countries:
+        cat_col = "native-country_" + category
+        if cat_col not in X.columns:
+            X[cat_col] = 0
+
+    X = X.sort_index(axis=1)
+    return X
+
 trainDf = clean2(dataFile)
 testDf = clean2(testFile)
 
@@ -116,5 +117,55 @@ for k in k_vals:
     kmeans = KMeans(n_clusters=k, random_state=42)
     clusters = kmeans.fit_predict(trainDf)
     print(f"KMeans with k={k}", clusters)
+
+
+# ============ Part 3 =============
+def clean3(fileName: str):
+    df = pd.read_csv(fileName, names=headers, na_values=['?'], skipinitialspace=True)
+    df.dropna(inplace=True)
+
+    # Remove all periods from the values
+    for column in categorical_features:
+        df[column] = df[column].str.rstrip('.')
+
+    # for each continious column, map it to if it is greater than the mean of that column
+    for column in continuous_features:
+        df[column] = df[column].astype(int)
+        mean = df[column].mean()
+        df[column] = df[column].map(lambda x: 1 if x > mean else 0)
+
+    X = df.drop(columns=["income"], axis=1)
+    Y = df["income"]
+    X = pd.get_dummies(X, drop_first=True)
+
+    # Add missing columns
+    for category in countries:
+        cat_col = "native-country_" + category
+        if cat_col not in X.columns:
+            X[cat_col] = 0
+
+    X = X.sort_index(axis=1)
+    return X, Y
+
+trainX, trainY = clean3(dataFile)
+testX, testY = clean3(testFile)
+
+# Save trainX to a file
+trainX.to_csv("trainX.csv", index=False)
+
+model = SVC(kernel="linear")
+
+print("Fitting SVM")
+
+model.fit(trainX, trainY)
+
+print("Fit SVM")
+
+predictions = model.predict(testX);
+report = classification_report(testY, predictions)
+
+print("\n\n\nSVM: ")
+
+print(report)
 
 
